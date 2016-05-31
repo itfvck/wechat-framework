1d6fa3234d6d967fed47cc5c3c9ace98e20f2f56
345280563@qq.com <345280563@qq.com>
2016-05-31 20:36:29 +0800

    Signed-off-by: 345280563@qq.com <345280563@qq.com>

--- wechatframework-demo/src/main/java/com/itfvck/wechatframework/service/MyService.java
+++ wechatframework-demo/src/main/java/com/itfvck/wechatframework/service/MyService.java
@@ -1,6 +1,6 @@
 package com.itfvck.wechatframework.service;
 
-import com.itfvck.wechatframework.web.support.WechatSupport;
+import com.itfvck.wechatframework.core.support.WechatSupport;
 
 /**
  * 用户自定义消息处理中心，若要该类起作用，则需配置在wem.xml中
