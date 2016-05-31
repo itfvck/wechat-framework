5f6bc634ffaec70c9a6d43469cfffa0dc1504439
vcdemon <345280563@qq.com>
2016-05-31 16:52:52 +0800

    Signed-off-by: vcdemon <345280563@qq.com>

--- wechatframework-demo/src/main/java/com/itfvck/wechatframework/service/MyService.java
+++ wechatframework-demo/src/main/java/com/itfvck/wechatframework/service/MyService.java
@@ -2,6 +2,12 @@ package com.itfvck.wechatframework.service;
 
 import com.itfvck.wechatframework.web.support.WechatSupport;
 
+/**
+ * 用户自定义消息处理中心，若要该类起作用，则需配置在wem.xml中
+ * 
+ * @author
+ *
+ */
 public class MyService extends WechatSupport {
 
 }
