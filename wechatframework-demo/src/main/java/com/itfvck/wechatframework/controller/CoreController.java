8fcf756f5574800c114d17355f89e18d58f9e967
vcdemon <345280563@qq.com>
2016-05-31 16:29:25 +0800

    Signed-off-by: vcdemon <345280563@qq.com>

---
+++ wechatframework-demo/src/main/java/com/itfvck/wechatframework/controller/CoreController.java
@@ -0,0 +1,12 @@
+package com.itfvck.wechatframework.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CoreController {
+    static Logger logger = LoggerFactory.getLogger(CoreController.class);
+    private static final long serialVersionUID = -2764698369628302107L;
+
+}
---
+++ wechatframework-demo/src/main/java/com/itfvck/wechatframework/service/MyService.java
@@ -0,0 +1,7 @@
+package com.itfvck.wechatframework.service;
+
+import com.itfvck.wechatframework.web.support.WechatSupport;
+
+public class MyService extends WechatSupport {
+
+}
