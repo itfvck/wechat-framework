846a6e86709e27f8294534a18a17d13ba494a58e
vcdemon <345280563@qq.com>
2016-06-30 08:57:19 +0800

    Signed-off-by: vcdemon <345280563@qq.com>

--- wechatframework-demo/src/main/java/com/itfvck/wechatframework/servlet/CoreServlet.java
+++
@@ -1,8 +0,0 @@
-package com.itfvck.wechatframework.servlet;
-
-import com.itfvck.wechatframework.web.servlet.WechatDefaultDispatcherServlet;
-
-public class CoreServlet extends WechatDefaultDispatcherServlet {
-    private static final long serialVersionUID = 1L;
-
-}
